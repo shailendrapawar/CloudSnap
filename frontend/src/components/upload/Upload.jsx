62d38a5a7c463857cb1c59045c34dcada160aeed
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-11 00:13:59 +0530

    constructing upload page

--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './upload.css'
+import src from './icon-final.png'
 const Upload = () => {
   return (
-    <div>Upload</div>
+    <main className='upload-body'>
+    <section className='upload-block'>
+
+    <label className="upload-left">
+      <input type='file' accept='image/*'hidden ></input>
+      
+      <img src={src}></img>
+      <p>Click or Drag and Drop image</p>
+    </label>  
+
+    <div className='upload-right'>
+      uploading list
+    </div>
+
+    </section>
+
+
+    <button className='upload-btn'>Sanp in cloud</button>
+    </main>
   )
 }
 
