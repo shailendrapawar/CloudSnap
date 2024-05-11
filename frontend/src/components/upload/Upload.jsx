f4a7d31d89468527d1cc50238a730b032bf91b55
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-11 14:19:21 +0530

    upload construction final done with reposnsive

--- frontend/src/components/upload/Upload.jsx
+++ frontend/src/components/upload/Upload.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './upload.css'
 import src from './icon-final.png'
 const Upload = () => {
+
+  const [files,setFiles]=useState([])
+
+ const handleChange=(e)=>{
+  e.preventDefault();
+
+  const inputFiles=Array.from(e.target.files)
+  
+  console.log("input files:-",inputFiles)
+  inputFiles.map((file,i)=>{
+    console.log("map hue",file.name)
+
+  })
+  inputFiles.splice(2,1)
+  console.log(inputFiles)
+ }
+
+
   return (
     <main className='upload-body'>
     <section className='upload-block'>
 
-    <label className="upload-left">
-      <input type='file' accept='image/*'hidden ></input>
+    <label className="upload-left" >
+      <input type='file' id='file-input' multiple onChange={(e)=>handleChange(e)} accept='image/*' hidden ></input>
       
       <img src={src}></img>
       <p>Click or Drag and Drop image</p>
     </label>  
 
     <div className='upload-right'>
-      uploading list
+      
     </div>
 
     </section>
