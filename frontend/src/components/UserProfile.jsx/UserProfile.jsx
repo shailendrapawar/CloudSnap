41bcd97cf564d7366dfb67c53a306dd7cd41b759
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-14 15:22:42 +0530

    user profile page left setup done

--- frontend/src/components/UserProfile.jsx/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx/UserProfile.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import './userProfile.css'
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <main className='user-body'>
+
+      <div className='user-block'>
+
+        <div className='user-left'>
+
+          <section className='userLeft-top'>
+            <img className='userProfile-img'></img>
+            <div className='profileImg-btn'>
+              <button className='removePic-btn'>Remove photo</button>
+              <button className='changePic-btn'>Change photo</button>
+            </div>
+          </section>
+
+          <section className='userLeft-middle'>
+            <input type='range' className='credentialRange'></input>
+            <input type='range' value={20} className='credentialRange'></input>
+          </section>
+
+          <button className='logout-btn'>Sign Out</button>
+
+        </div>
+        <div className='user-right'>user irght </div>
+
+      </div>
+
+
+    </main>
   )
 }
 
