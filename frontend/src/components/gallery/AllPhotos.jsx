6484ff0fa5258c9605ada569274c5c8553d54792
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-22 12:10:48 +0530

    work in fav section

--- frontend/src/components/gallery/AllPhotos.jsx
+++ frontend/src/components/gallery/AllPhotos.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import src from './photos/photo1.png'
 const AllPhotos = () => {
+
+
+  const photos=[
+    {
+      src:"/photos/photo1.png",
+      isFav:true
+    },
+    {
+      src:"/photos/photo2.png",
+      isFav:true
+    },
+    {
+      src:"/photos/photo3.png",
+      isFav:true
+    }
+    ,
+    {
+      src:"/photos/photo4.png",
+      isFav:true
+    },
+    {
+      src:"/photos/photo5.png",
+      isFav:true
+    },
+    {
+      src:"/photos/photo6.png",
+      isFav:true
+    }
+  ]
+
+
+  console.log(photos)
+  
   return (
-    <div className='relative'>AllPhotos</div>
+    <div className='relative allPhoto-body'>
+    {photos.map((v)=>{
+    
+      return <img src={v.src} ></img>
+
+    })}
+
+    </div>
   )
 }
 
