88d8bf7707f8dec7bc8ffb00f99383bb4d962e21
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-24 21:21:05 +0530

    auth pages edited

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,20 +10,25 @@ import Upload from './components/upload/Upload.jsx';
 import UserProfile from './components/UserProfile.jsx/UserProfile.jsx';
 import Favourites from './components/gallery/Favourites.jsx';
 import AllPhotos from './components/gallery/AllPhotos.jsx';
+import LoginPage from './components/authPages/LoginPage.jsx';
+import RegisterPage from './components/authPages/RegisterPage.jsx';
 
 const myRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
+    <Route>
+      <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />}></Route>
 
       <Route path='gallery' element={<Gallery />}>
         <Route path='' element={<AllPhotos/>} />
         <Route path='favourites' element={<Favourites />} />
-        {/* <Route path='upload' element={<Upload/>} /> */}
       </Route>
 
       <Route path='upload' element={<Upload />} />
       <Route path='/userProfile' element={<UserProfile />} />
+    </Route>
+    <Route path='/login' element={<LoginPage/>} ></Route>
+    <Route path='/register' element={<RegisterPage/>} ></Route>
     </Route>
 
   )
