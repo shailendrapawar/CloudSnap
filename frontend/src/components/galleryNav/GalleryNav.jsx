c337f45b5898dd7e78f4eaef12b19498610269ff
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-24 10:50:21 +0530

    taliwind added to gallery navigation

--- frontend/src/components/galleryNav/GalleryNav.jsx
+++ frontend/src/components/galleryNav/GalleryNav.jsx
@@ -9,7 +9,7 @@ const GalleryNav = () => {
     const navigate=useNavigate()
 
   return (
-    <nav className='gallery-nav'>
+    <nav className='gallery-nav bg-[#00334eb1] text-white absolute flex flex-col justify-evenly items-center '>
         <NavLink  className={({isActive})=>isActive?"activeClass":"none"} to='/gallery/favourites'><FaHeart/></NavLink>
         <NavLink className={({isActive})=>isActive?"activeClass":"none"} to='/gallery/'><GrGallery/></NavLink>
         <NavLink className={({isActive})=>isActive?"activeClass":"none"} to='/upload'><MdAddPhotoAlternate/></NavLink>
