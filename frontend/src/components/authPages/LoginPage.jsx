52bd8a9ca9170951ec35e38fcabbd3f1793fc389
shailendrapawar <shailendrapawar980@gmail.com>
2024-05-27 12:23:26 +0530

    login work started

--- frontend/src/components/authPages/LoginPage.jsx
+++ frontend/src/components/authPages/LoginPage.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-
+import { FaRegUser } from "react-icons/fa";
+import { AiOutlineUnlock } from "react-icons/ai";
 const LoginPage = () => {
   return (
     <div className='login-page'>
-    <main className='login-body'>
-    <section className='login-left'>left</section>
-    <section className='login-right'>right</section>
-    </main>
+      <main className='login-body'>
+
+        <section className='login-left'>
+          <p className='login-link'>New to CloudSnap <b>register here</b>?</p>
+        </section>
+
+
+        <section className='login-right'>
+        <h1>Sign-in</h1>
+        <p>enter the credentials</p>
+          <form className='login-form'>
+          <div className='name-div'><i><FaRegUser /></i><input type='text' placeholder='enter your full name'></input></div>
+          <div className='password-div'><i><AiOutlineUnlock /></i><input type='text' placeholder='enter password'></input></div>
+          </form>
+        </section>
+      </main>
 
     </div>
   )
